910c145462eab7b426992e59ac8acb464e7e9752
Thomas van der Veen <thomas@bluefeniks.com>
2022-07-15 21:21:37 +0200

    solved two problems

--- src/functional/tv.clj
+++ src/functional/tv.clj
@@ -1,5 +1,24 @@
 (ns functional.tv
+  (:require [clojure.string :as str])
   (:gen-class))
+
+;; 20220715
+(defn regroup [string size]
+  (let [replaced (str/replace string #"-" "")
+        length (count replaced) ]
+    ;;(println "here: " (< size length) (zero? (rem length size))    (or (and (< size length) (zero? (rem length size))) (not (< size length)) ) )
+    (if (or (and (< size length) (zero? (rem length size))) (not (< size length)))
+      (str/join "-" (map str/join (partition-all size replaced )))
+      (let [f (first replaced)
+            r (rest replaced) ]
+        (str f "-" (str/join "-" (map str/join (partition-all size (str/join "" r)))))))))
+
+
+;; 20220529
+(defn reverse-words [words]
+  (->> (str/split words #" ")
+       (reverse)
+       (str/join " ")))
 
 (defn greet
   "Callable entry point to the application."
--- test/functional/tv_test.clj
+++ test/functional/tv_test.clj
@@ -1,7 +1,24 @@
 (ns functional.tv-test
-  (:require [clojure.test :refer :all]
-            [functional.tv :refer :all]))
+  (:require [clojure.test :as test]
+            [functional.tv :as tv]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(test/deftest a-test
+  (test/testing "FIXME, I fail."
+    (test/is (= 1 1))))
+
+;; 20220529
+(test/deftest reverse-words-test
+  (test/testing ""
+    (test/is (= "name my is Eric." (tv/reverse-words "Eric. is my name")))
+    (test/is (= "hello" (tv/reverse-words "hello")))
+    (test/is (= "you love I" (tv/reverse-words "I love you")))))
+
+;; 20220715
+(test/deftest regroup-license-plate 
+  (test/testing ""
+    (test/is (= "A-5GG-B88" (tv/regroup "A5-GG-B88" 3)))
+    (test/is (= "A-5G-GB-88" (tv/regroup "A5-GG-B88" 2)))
+    (test/is (= "67-76" (tv/regroup "6776" 2)))
+    (test/is (= "F-3-3" (tv/regroup "F33" 1))) 
+    (test/is (= "IIO" (tv/regroup "IIO" 7)))
+                ))
